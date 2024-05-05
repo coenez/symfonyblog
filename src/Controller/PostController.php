c44fc1afd94e3647a0b31a4b3d5c8367e087ff79
coenez <ccpstals@hotmail.com>
2024-05-05 11:56:48 +0200

    Add Post overview and post detail structures

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Post;
+use App\Repository\PostRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,10 +11,19 @@ use Symfony\Component\Routing\Attribute\Route;
 class PostController extends AbstractController
 {
     #[Route('/post', name: 'app_post')]
-    public function index(): Response
+    public function index(PostRepository $postRepository): Response
     {
         return $this->render('post/index.html.twig', [
-            'controller_name' => 'PostController',
+            'posts' => $postRepository->findAll(),
+        ]);
+
+    }
+
+    #[Route('/post/{id<\d>}', name: 'app_post_show')]
+    public function show(Post $post): Response
+    {
+        return $this->render('post/show.html.twig', [
+            'post' => $post
         ]);
     }
 }
